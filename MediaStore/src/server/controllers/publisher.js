93534069033437f7f7768a75d938640f50bfe5a5
Petre Leonard Macamete <petreleonartdos@gmail.com>
2019-07-25 17:58:40 +0300

    Add publishers and publisher

--- MediaStore/src/server/controllers/publisher.js
+++ MediaStore/src/server/controllers/publisher.js
@@ -5,30 +5,57 @@ class PublisherController {
 
   // Aggretation request
   get(name, done) {
-    this.model.aggregate([
-      {
-        $match: {
-          Publisher: name
-        }
-      },
-      // Sort in descending order
-      {
-        $sort: {
-          'YEAR': -1
-        }
-      },{
-      $group: {
-        _id: '$Publisher',
-        Publisher: { "$first": '$Publisher' },
-        publications: {
-          $push: {
-            id: '$_id',
-            title: '$Title',
-            year: "$YEAR"
+    if (name) {
+      this.model.aggregate([
+        {
+          $match: {
+            Publisher: name
+          }
+        },
+        // Sort in descending order
+        {
+          $sort: {
+            'YEAR': -1
+          }
+        }, {
+          $group: {
+            _id: '$Publisher',
+            Publisher: { "$first": '$Publisher' },
+            publications: {
+              $push: {
+                id: '$_id',
+                title: '$Title',
+                year: "$YEAR"
+              }
+            }
           }
         }
-      }}
-    ]).exec(done);
+      ]).exec(done); 
+    }
+    else{
+      this.model.aggregate([
+        {
+          $match: {
+            Publisher: { "$ne": "" }
+          }
+        },
+        {
+          $group: {
+            _id: '$Publisher',
+            Publisher: { "$first": '$Publisher' },
+            minPublicationYear: { "$min": "$YEAR" },
+            maxPublicationYear: { "$max": "$YEAR" },
+            count: { $sum: 1 }
+          }
+        },
+        {
+          $sort: {
+            count: -1,
+            maxPublicationYear: -1
+          }
+        }
+      ]).limit(50).exec(done);
+    }
   }
 
 }
